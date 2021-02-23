2d09583e6dc19601446fb04336a5b4f54db5a0f5
hyperhopi <hyperhopi@github.com>
2021-02-23 13:49:57 +0100

    we finished basic css and then started js

---
+++ script.js
@@ -0,0 +1,17 @@
+const textElement = document.getElementById('question');
+const optionButtons = document.getElementById('option-buttons');
+
+let state = {}
+
+function startGame() {
+    state = {}
+    showTextNode(1)
+}
+
+function showTextNode() {
+
+}
+
+function selectOption() {
+
+}
