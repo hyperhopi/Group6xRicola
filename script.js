1f673a6341d4b1bee6c5907f6459d7d1b58ccdac
lingi524 <lingi524@student.liu.se>
2021-02-23 14:25:08 +0100

    Started to add text-nodes, options, etc etc.

--- script.js
+++ script.js
@@ -1,17 +1,69 @@
 const textElement = document.getElementById('question');
 const optionButtons = document.getElementById('option-buttons');
 
-let state = {}
+let natureState = {}
+    // let peopleState = {}
 
 function startGame() {
     state = {}
     showTextNode(1)
 }
 
-function showTextNode() {
+function showTextNode(textNodeIndex) {
+    const textNode = textNodes.find(textNode => textNode.id === textNodeIndex)
+    textElement.innerText = textNode.text
+}
+
+function selectOption(option) {
 
 }
 
-function selectOption() {
+const textNodes = [{
+        id: 1,
+        text: "Welcome to the Ricola Factory!",
+        options: [{
+                text: "Nature option",
+                setNatureState: { naturePoint: true },
+                nextText: 2
+            },
+            {
+                text: "People option",
+                // setPeopleState: {}
+                nextText: 2
+            },
+            {
+                text: "Business option",
+                nextText: 2
+            },
+            {
+                text: "X option",
+                nextText: 2
+            }
+        ]
+    },
+    {
+        id: 2,
+        text: "Welcome to the Ricola Factory VERSION 2!",
+        options: [{
+                text: "Nature option",
+                setNatureState: { naturePoint: true },
+                nextText: 3
+            },
+            {
+                text: "People option",
+                // setPeopleState: {}
+                nextText: 3
+            },
+            {
+                text: "Business option",
+                nextText: 3
+            },
+            {
+                text: "X option",
+                nextText: 3
+            }
+        ]
+    },
+]
 
-}
+startGame();
