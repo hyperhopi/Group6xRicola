26e9b5cf8fbd2d58939b8269b32af7bacc0e2874
lingi524 <lingi524@student.liu.se>
2021-02-26 10:53:45 +0100

    Added a popup box when the last option is reached

--- script.js
+++ script.js
@@ -1,16 +1,14 @@
 const textElement = document.getElementById('question');
 const optionButtonsElement = document.getElementById('option-buttons');
+const closeButton = document.getElementById('closeButton').addEventListener("click",showPopUp);
 let naturePoint = 0;
 let businessPoint = 0;
 let peoplePoint = 0;
 
 
-let natureState = {}
-    // let peopleState = {}
-
 function startGame() {
-    state = {}
-    showTextNode(1)
+    state = {};
+    showTextNode(1);
 }
 
 function incrementNaturePoint() {
@@ -25,20 +23,31 @@ function incrementPeoplePoint() {
     peoplePoint++;
 }
 
+//Show or hide the popup message
+
+function showPopUp() {
+    var popup = document.getElementById("myPopup");
+    popup.classList.toggle("show");
+  }
+
+
+
+  //Show or hide text and buttons
+
 function showTextNode(textNodeIndex) {
-    const textNode = textNodes.find(textNode => textNode.id === textNodeIndex)
-    textElement.innerText = textNode.text
+    const textNode = textNodes.find(textNode => textNode.id === textNodeIndex);
+    textElement.innerText = textNode.text;
     while (optionButtonsElement.firstChild) {
-        optionButtonsElement.removeChild(optionButtonsElement.firstChild)
+        optionButtonsElement.removeChild(optionButtonsElement.firstChild);
     }
 
     textNode.options.forEach(option => {
         if (showOption(option)) {
             const button = document.createElement("button")
-            button.innerText = option.text
-            button.classList.add("btn")
-            button.addEventListener('click', () => selectOption(option))
-            optionButtonsElement.appendChild(button)
+            button.innerText = option.text;
+            button.classList.add("btn");
+            button.addEventListener('click', () => selectOption(option));
+            optionButtonsElement.appendChild(button);
         }
     })
 }
@@ -52,7 +61,7 @@ function showOption(option) {
 }
 
 function selectOption(option) {
-    let nextTextNodeId = option.nextText
+    let nextTextNodeId = option.nextText;
 
     if (option.typeOfPoint==="nature"){
         incrementNaturePoint();
@@ -68,28 +77,28 @@ function selectOption(option) {
     } 
 
     if (nextTextNodeId <= 0) {
-        return startGame() //change to showMap later
+       // return startGame() //change to showMap later
+       showPopUp();
     }
 
     state = Object.assign(state, option.setNatureState)
-    showTextNode(nextTextNodeId)
+    showTextNode(nextTextNodeId);
 }
 
 
-
+//Where all the content in the game is stored
 const textNodes = [{
         id: 1,
         text: "Welcome to the Ricola Factory!",
         options: [{
                 text: "Nature option",
-                setNatureState: { naturePoint: true },
                 typeOfPoint: "nature",
                 nextText: 2
             },
             {
                 text: "People option",
-                // setPeopleState: {}
                 typeOfPoint: "people",
+                numberofPoints: 3,
                 nextText: 2
             },
             {
@@ -110,9 +119,8 @@ const textNodes = [{
         options: [{
                 text: "Nature again",
                 typeOfPoint: "nature",
-                // requiredState: (currentState) => currentState.naturePoint,
-                // setState: { naturePoint: false, sword: true },
-                nextText: 3
+                typeOfPoint: "nature",
+                nextText: -1 //just to test the popup
             },
             {
                 text: "People option",
